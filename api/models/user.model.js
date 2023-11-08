67d7786b85f644bee6f0aeea8224e8f61e856a8d
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-11-08 19:06:06 +0530

    created user model i.e schema  in api/models

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    Email{
+        type:String,
+        required:true,
+        unique:true
+    },
+    Password:{
+        type:String,
+        required:true
+    }
+} , { timestamps : true });
+
+const User= new mongoose.model('User',userSchema);
+
+export default User;
