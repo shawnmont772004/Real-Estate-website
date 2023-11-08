e8cbd30c7cb2ec5becdd090b65e41bea2cdac893
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-11-08 15:43:07 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import Profile from "./pages/Profile.jsx"
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />}/>
+          <Route path="/SignIn" element={<SignIn />}/>
+          <Route path="/SignUp" element={<SignUp />}/>
+          <Route path="/Profile" element={<Profile />}/>
+          <Route path="*" element={<div>Page not Found</div>}/>
+        </Routes>
+      </Router>
+    </div>
+
+    </>
   )
 }
 
