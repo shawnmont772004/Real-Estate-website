29b2e7918b1ea29face50356c46fbd8a70297b6e
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-11-08 18:04:26 +0530

    connected to Database Real_Estate also installed dependency mongoose & ignored .env

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app=express()
+dotenv.config()
+const app=express();
 
+mongoose
+    .connect(process.env.REAL_ESTATE_DB)
+    .then(()=>{console.log("Connected to the database!")})
+    .catch((err) => {console.log(err)})
 app.listen(3000, () => {
-    console.log("Server is runnning on 3000")
+    console.log("Server is runnning on 3000");
 })
